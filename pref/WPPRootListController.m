77cb9bf5bdaef1ea56c8dedc942ef646b343fa70
Hao Nguyen <hao.ict56@gmail.com>
2020-07-26 23:05:57 +0900

    Fixes footer subtitles, refactor code

--- pref/WPPRootListController.m
+++ pref/WPPRootListController.m
@@ -15,7 +15,7 @@
   return self;
 }
 
--(void)apply {
+- (void)apply {
   [HCommon killProcess:@"Weather" viewController:self alertTitle:@TWEAK_TITLE message:[self localizedItem:@"DO_YOU_REALLY_WANT_TO_KILL_WEATHER"] confirmActionLabel:[self localizedItem:@"CONFIRM"] cancelActionLabel:[self localizedItem:@"CANCEL"]];
 }
 
